401765cf3c9ac60d68c8b55aec16ec33a4566cfc
Skander <skandergorgi007@gmail.com>
2026-02-10 21:28:20 +0100

    Added redis deployment conf and users

--- redis/entrypoint.sh
+++ redis/entrypoint.sh
@@ -1,4 +1,3 @@
+#!/bin/sh
 envsubst < /usr/local/etc/redis/users.acl.template > /usr/local/etc/redis/users.acl
-
-# 2. Lancer Redis avec les arguments passés au script (le CMD du Dockerfile)
 exec "$@"
